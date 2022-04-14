ed7bb2a1690eb88ce46799f201ae957ec82d3026
maguowei <imaguowei@gmail.com>
2022-04-14 17:43:31 +0800

    ensure countdown return >=0

--- bots/countdown.py
+++ bots/countdown.py
@@ -5,7 +5,9 @@ import fire
 def countdown() -> int:
     end_day = datetime.date(2023, 12, 31)
     today = datetime.date.today()
-    return (end_day - today).days
+    
+    delta = end_day - today
+    return delta.days if delta.days > 0 else 0
 
 
 if __name__ == "__main__":
